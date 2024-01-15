acef2f8d93a618f1f42072c192126f2381ea838e
Juana <juanapaula.siemel@sanviatorvalladolid.com>
2024-01-15 10:28:36 +0100

    Actualización con Pantalla Tareas

--- app/src/main/java/com/example/grupo3/ActivityNuevaTarea.java
+++ app/src/main/java/com/example/grupo3/ActivityNuevaTarea.java
@@ -2,10 +2,20 @@ package com.example.grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.time.LocalDate;
+
+import Tablas.Modulo;
+import Tablas.Tarea;
 
 public class ActivityNuevaTarea extends AppCompatActivity {
 
@@ -19,11 +29,58 @@ public class ActivityNuevaTarea extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                AlertDialog.Builder builder=new AlertDialog.Builder(v.getContext());
+
+                builder.setTitle("Mensaje Informativo");
+                builder.setMessage("Estás a punto de guardar una nueva tarea, si estás seguro haz clic en 'aceptar'");
+                builder.setIcon(android.R.drawable.btn_star_big_on);
+
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Tarea t=new Tarea();
+                        int idTarea=Integer.parseInt(String.valueOf((EditText)findViewById(R.id.idTarea)));
+                        String moduloTarea=String.valueOf((EditText)findViewById(R.id.moduloTarea));
+                        String nombreTarea=String.valueOf((EditText)findViewById(R.id.nombreTarea));
+                        //LocalDate fechaEntrega= LocalDate.parse(String.valueOf((EditText)findViewById(R.id.fechaEntrega)));
+
+
+                       t.setId(idTarea);
+                       t.setModulo(moduloTarea);
+                       t.setTarea(nombreTarea);
+                       //t.setFechaEntrega();
+
+
+                        //DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference().child("Modulos");
+                        //dbRef.push().setValue(m);
+
+                        Intent pantallaPrincipal=new Intent(ActivityNuevaTarea.this, MenuPrincipal.class);
+                        startActivity(pantallaPrincipal);
+                    }
+                });
+                builder.setNegativeButton("No aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        View padre=(View) v.getParent();
+                        Snackbar barra= Snackbar.make(padre,"Si no aceptas modifica algún campo",Snackbar.LENGTH_SHORT);
+                        barra.show();
+                    }
+                });
+
+                builder.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        View padre=(View) v.getParent();
+                        Snackbar barra= Snackbar.make(padre,"Has cancelado el registro",Snackbar.LENGTH_SHORT);
+                        barra.show();
+                    }
+                });
+                AlertDialog cuadroDialogo = builder.create();
+                cuadroDialogo.show();
+
             }
         });
+
     }
 }
-
-
-
-
--- app/src/main/java/com/example/grupo3/ActivityNuevoModulo.java
+++ app/src/main/java/com/example/grupo3/ActivityNuevoModulo.java
@@ -2,12 +2,20 @@ package com.example.grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import Tablas.Modulo;
 
 public class ActivityNuevoModulo extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,55 @@ public class ActivityNuevoModulo extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                AlertDialog.Builder builder=new AlertDialog.Builder(v.getContext());
+
+                builder.setTitle("Mensaje Informativo");
+                builder.setMessage("Estás a punto de guardar un nuevo módulo, si estás seguro haz clic en 'aceptar'");
+                builder.setIcon(android.R.drawable.btn_star_big_on);
+
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Modulo m=new Modulo();
+                        String nombre=String.valueOf((EditText)findViewById(R.id.nombreModulo));
+                        String ciclo=String.valueOf((EditText)findViewById(R.id.ciclo));
+                        String usuario=String.valueOf((EditText)findViewById(R.id.usuario));
+
+                        m.setModulo(nombre);
+                        m.setCiclo(ciclo);
+                        m.setUsuario(usuario);
+
+
+                        //DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference().child("Modulos");
+                        //dbRef.push().setValue(m);
+
+                        Intent pantallaPrincipal=new Intent(ActivityNuevoModulo.this, MenuPrincipal.class);
+                        startActivity(pantallaPrincipal);
             }
         });
+                builder.setNegativeButton("No aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        View padre=(View) v.getParent();
+                        Snackbar barra= Snackbar.make(padre,"Si no aceptas modifica algún campo",Snackbar.LENGTH_SHORT);
+                        barra.show();
+                    }
+                });
+
+                builder.setNeutralButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        View padre=(View) v.getParent();
+                        Snackbar barra= Snackbar.make(padre,"Has cancelado el registro",Snackbar.LENGTH_SHORT);
+                        barra.show();
+                    }
+                });
+                AlertDialog cuadroDialogo = builder.create();
+                cuadroDialogo.show();
+
+            }
+        });
+
     }
 }
